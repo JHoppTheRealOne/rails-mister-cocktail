b00892c4d79dbefe553e613f1a577880a9274bf6
JHoppTheRealOne <thom.malrain@gmail.com>
2019-05-24 12:09:53 +0200

    Add dotenv - Protect my secret data in .env file

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,22 +1,36 @@
 class DosesController < ApplicationController
-  def index
-  end
+  before_action :set_cocktail, only: [:new, :create]
 
   def new
+    @ingredients = Ingredient.all
+    @dose = Dose.new
   end
 
   def create
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
+    @ingredients = Ingredient.all
+    @dose = Dose.new(doses_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def doses_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   # get 'cocktails/update'
   # get 'cocktails/destroy'
   resources :cocktails do
-    resources :doses
+    resources :doses, only: [:new, :create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doses, only: :destroy
